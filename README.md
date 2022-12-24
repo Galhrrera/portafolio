88c89f40315472b3ad184b6084051f14e7d46bd4
galhrrera <juan.galeanoherrera@gmail.com>
2022-12-23 23:59:28 -0500

    Avances en menu burguer

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # portafolio
 porafolio de desarrollador
+
+https://www.youtube.com/watch?v=tcskp-ncN0I
+
+https://www.youtube.com/watch?v=gbkgQfIoWoM
--- www/js/main.js
+++ www/js/main.js
@@ -3,9 +3,13 @@ document.querySelector(".burguer_menu").addEventListener("click", activeBurguerM
 var line1 = document.querySelector(".line1_bar_menu");
 var line2 = document.querySelector(".line2_bar_menu");
 var line3 = document.querySelector(".line3_bar_menu");
+var menu = document.querySelector(".menu");
 
 function activeBurguerMenu() {
     line1.classList.toggle("activeline1_bar_menu");
     line2.classList.toggle("activeline2_bar_menu");
     line3.classList.toggle("activeline3_bar_menu");
+    console.log("Cambio el manu");
+    menu.classList.toggle("menu_burguer");
+    console.log("Apareció el manu");
 }
