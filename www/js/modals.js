289b003de82dd5a0b2d2c3c8748cf895ef4669ef
galhrrera <juan.galeanoherrera@gmail.com>
2022-12-30 17:09:56 -0500

    Skills listas con sus respectivos modales

--- www/js/modals.js
+++ www/js/modals.js
@@ -57,42 +57,115 @@ function showModal(element) {
         case "mongo_img":
             modal_img = document.getElementById("modal_img").src = "./www/img/mongo400.png";
             modal_title.innerHTML = `Mongo DB`;
-            modal_text.innerHTML = `El motor de bases de datos <em>SQL</em> con el que aprendí todos los fundamentos de las bases de datos, comenzando desde la normalización de los datos hasta la construcción de sentencias robustas, lógica almacenada, creación de vistas y demás.<br>
-            Al día de hoy es el motor de bases de datos que elegiría si tuviera que elegir un motor SQL.<br>
-            Como dato adicional, en temas de bases de datos SQL, he tenido experiencia con:
-            <li>MySQL</li>
-            <li>CrateDB</li>`
+            modal_text.innerHTML = `Mi experiencia con <em>bases de datos No SQL</em> ha sido con este motor: <em>MongoDB</em>.<br>
+            Existen 2 proyectos en los que he interactuado con mongo:
+            <li>Fiware UPB</li>
+            <li>Proyecto personal con python para practicar la implementación de aplicaciones con bases de datos embebidas.</li><br>
+            Por supuesto, también he interactuado con <em>Compass</em>, como GUI para <em>MongoDB</em>.`
             modal.classList.add("modal_show");
             break;
         case "linux_img":
-            alert("mongo_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/linux400.png";
+            modal_title.innerHTML = `LINUX`;
+            modal_text.innerHTML = `
+            No por nada cuento con una distribución de <em>linux</em> instalada en mi ordenador personal (Parrot Security).<br>
+            Conozco el potencial de <em>linux</em> y tengo experiencia con sistemas operativos basados en <em>linux</em> y su terminal.
+            `
+            modal.classList.add("modal_show");
             break;
         case "html_img":
-            alert("html_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/html400.png";
+            modal_title.innerHTML = `HTML`;
+            modal_text.innerHTML = `Mis últimos proyectos se han desarrollado utilizando <em>HTML</em>, <em>CSS</em> y <em>JS</em>.<br>
+            El desarrollo web ha sido una de las opciones que más han llamado mi atención en los últimos meses, por lo que he dedicado tiempo a aprender y a perfeccionar poco a poco mis conocimientos y habilidades con <em>HTML</em>.<br>
+            Algunos de los proyectos que más eh disfrutado construir son aplicaciones con bases de datos embebidas y este sitio.`
+            modal.classList.add("modal_show");
             break;
         case "css_img":
-            alert("css_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/css400.png";
+            modal_title.innerHTML = `CSS`;
+            modal_text.innerHTML = `Jamás podría trabajar con <em>HTML</em> sin hacer uso de <em>CSS</em>.<br>
+            Si bien el diseño, uso de colores, disposiciones y demás no son mi fuerte, cuento con las habilidades necesarias hacer uso de <em>CSS</em> de forma decente.<br>
+            Con <em>CSS</em> he tenido experiencia a la hora de diseñar aplicaciones <em>responsive</em> gracias al usod e <em>media querys</em>, he realizado <em>carruseles de imágenes</em> e incluso tengo experiencia y conocimientos sobre las <em>propiedades personalizadas</em> de <em>CSS</em> (Variables).`
+            modal.classList.add("modal_show");
             break;
         case "sass_img":
-            alert("sass_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/sass400.png";
+            modal_title.innerHTML = `SASS`;
+            modal_text.innerHTML = `<em>SASS</em> y <em>SCSS</em> como preprocesadores de <em>CSS</em> ha sido mi aprendizaje más reciente.<br>
+            Cuando buscaba rutas de aprendizaje para ser desarrollador, y encontré los preprocesadores de <em>CSS</em>, entendí que eran tecnologías que debía aprender.<br>
+            El primer proyecto que implementé utilizando <em>SASS</em> es este portafolio, en el cual practiqué el uso de:
+            <li><em>Variables</em></li>
+            <li><em>Nesting</em></li>
+            Adicionalmente, tengo conocimientos sobre <em>Partials</em>, <em>Modules</em>, <em>Mixins</em>, <em>Extend/Inheritance</em> y <em>Operators</em>.`
+            modal.classList.add("modal_show");
             break;
         case "wordpress_img":
-            alert("wordpress_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/wordpress400.png";
+            modal_title.innerHTML = `WORDPRESS`;
+            modal_text.innerHTML = `El uso de los <em>CMS</em>, o gestores de contenido, ha estado dentro de mis radares.<br>
+            El uso de plantillas, plugins y el acompañamiento de buenos grupos de trabajo me han permitido desarrollar aplicaciones tipo <em>blog</em> e <em>eCommerce</em>.<br>
+            Mi grano de arena más reciente con el uso de <em>Wordpress</em> como <em>CMS</em> ha sido para la página web del <a href="https://holamundo.co/" title="Fiware iHub UPB" target="_blank" rel="noopener noreferrer">Fiware iHub UPB</a>". En este sitio se me otorgó la tarea de ingresar un par de entradas al blog del sitio (El sitio del iHub UPB sigue en construcción a la fecha de desarrollo de este portafolio).
+            Algunas de mis implementaciones han incluido <em>Botón de WPP</em>, <em>Pasarelas de pago</em>, <em>Páginas de contacto</em> y otros.`
+            modal.classList.add("modal_show");
             break;
         case "aws_img":
-            alert("aws_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/aws400.png";
+            modal_title.innerHTML = `AWS`;
+            modal_text.innerHTML = `Los servicios en la nube son temas que atraparon mi atención desde la primera vez que ingresé a una <em>EC2</em> de <em>AWS</em>.<br>
+            Mi experiencia con la nube de <em>AWS</em> ha sido adquirida con mucho cariño, debido al gusto que adquirí al interactuar con la nube.<br>
+            Al día de hoy sigo utilizando la nube de <em>AWS</em> para probar algunos <em>servicios</em>, <em>contenedores</em> y otros.<br>
+            Mi experiencia con la nube consta de:
+            <li>Administración de <em>EC2, VPCs, grupos y reglas de seguridad</em>.</li>
+            <li><em>SSH</em></li>
+            <li>Configuración de <em>IP elásticas</em></li>
+            Entre otros.
+            `
+            modal.classList.add("modal_show");
             break;
         case "docker_img":
-            alert("docker_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/docker400.png";
+            modal_title.innerHTML = `AWS`;
+            modal_text.innerHTML = `Mi experiencia con docker me ha llevado incluso a realizar distintas capacitaciones para grupos con los que he trabajado sobre el uso de los <em>contenedores</em>.<br>
+            Con los <em>contenedores de docker</em> he desplegado servicios de <em>bases de datos SQL y NoSQL</em>, <em>aplicaciones web</em> y SO<em> linux</em>, entre otros.
+            Mi experiencia abarca el uso de:
+            <li><em>Docker</em></li>
+            <li><em>Docker Compose</em></li>
+            Mi experiencia laboral más reciente para el Fiware iHub de la UPB ha requerido el uso de contenedores para el despliegue de la arquitectura <em>Fiware</em>, plataforma para soluciones inteligentes.
+            `
+            modal.classList.add("modal_show");
             break;
         case "fiware_img": 
-            alert("fiware_img");
+        modal_img = document.getElementById("modal_img").src = "./www/img/fiware400.png";
+        modal_title.innerHTML = `FIWARE`;
+        modal_text.innerHTML = `
+        Mi trabajo más reciente se centró en la implementación de la plataforma <em>Fiware</em> para la instauración del Fiware iHub UPB en la ciudad de Medellín (4to iHub de Latino América).<br>
+        Sé como implementar Fiware con enfoque al análisis de datos en tiempo real de distintos dispositivos inteligentes, sensores y dispositivos IoT, los cuales están distribuidos no solo en el ecocampus UPB sino al rededor de país.<br>
+        la solución <em>Fiware</em> implementada requiere el uso de <em>contenedores</em> y <em>peticiones HTTP</em>, <em>Mongo DB</em>, <em>CrateDB</em>, <em>Grafana</em> y otros.<br>
+        En el ecocampus de la UPB se dedicó un servidor virtualizado para desplegar los contenedores con la arquitectura completa de la solución.<br>
+        El proyecto Fiware iHub UPB va de la mano con la alcaldía de Medellín, ya que los planes son implusar el desarrollo de Medellín como <em>ciudad inteligente</em>.
+        `
+        modal.classList.add("modal_show");
             break;
         case "grafana_img":
-            alert("grafana_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/grafana400.png";
+            modal_title.innerHTML = `GRAFANA`;
+            modal_text.innerHTML = `
+            Grafa es un software libre que permite, entre otros, el monitoreo de datos en tiempo real.<br>
+            Grafana es una herramienta poderosa para crear distintos tipos de gráficos de datos a partir de un gran número de fuentes de datos, incluyendo series de tiempo.<br>
+            Mi experiencia con grafana, además de su despliegue a través de contenedores, ha sido configurar diversas fuentes de datos, crear un gran número de tableros de control en tiempo real y acompañar el proceso de monitorización de datos provenientes de distintos dispositivos inteligentes, sensores, dispositivos IoT,e staciones meteorológicas, entre otros según las necesiades.
+            `
+            modal.classList.add("modal_show");
             break;
         case "git_img":
-            alert("git_img");
+            modal_img = document.getElementById("modal_img").src = "./www/img/git400.png";
+            modal_title.innerHTML = `GIT - GITHUB`;
+            modal_text.innerHTML = `
+            El uso de <em>repositorios</em> por supuesto viene dentro del abanico de habilidades con las que cuento.
+            Muchos de los proyectos en los que he trabajado han estado controlados a través de <em>GitHub</em>, lo cual me ha llevado a aprender a utilizar la herramienta para la administración de repositorios, uso de ramas e <em>issues</em>.<br>
+            Algunos de mis proyectos personales, y otros en los que he trabajado de otros desarrolladores, se encuentran almacenados en mi <a href="https://github.com/Galhrrera" title="GitHub personal" target="_blank" rel="noopener noreferrer">perfil de GitHub personal</a>.
+            `
+            modal.classList.add("modal_show");
             break;
         
     }
