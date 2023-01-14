68912f54a291896b0745ad7e34efc5c9d3c4bffc
galhrrera <juan.galeanoherrera@gmail.com>
2023-01-13 23:42:10 -0500

    Avances en certificados y logros

---
+++ www/js/slide.js
@@ -0,0 +1,34 @@
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  slideIndex += n;
+  showSlides();
+}
+
+function currentSlide(n) {
+  slideIndex = n;
+  showSlides();
+}
+
+function showSlides() {
+    alert("Entra en el método");
+  var i;
+  var slides = document.getElementsByTagName("section")[0].getElementsByTagName("div");
+  var dots = document.getElementsByTagName("section")[0].getElementsByClassName("dot");
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
