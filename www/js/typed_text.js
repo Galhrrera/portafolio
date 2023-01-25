be0c53e842f6eb1502ad36e224523b4b031ac374
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-01-25 17:24:41 -0500

    Se agregó la funcionalidad del tipado automático a los span con los mensajes Estudiante, Desarrollador, Ingeniero de Sistemase Informática

---
+++ www/js/typed_text.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto_typed", {
+    strings: ["Estudiante", "Desarrollador", "Ingeniero de Sistemas e Informática"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+})
+
+var typed = new Typed(".auto_typed2", {
+    strings: ["Estudiante", "Desarrollador", "Ingeniero de Sistemas e Informática"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+})
